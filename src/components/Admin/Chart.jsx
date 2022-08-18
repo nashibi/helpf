34ed501587b1f817ed376d8b74e4304c015c5007
mohammad <mamad9494s@gmail.com>
2022-08-18 03:05:43 -0700

    helf v3

--- src/components/Admin/Chart.jsx
+++ src/components/Admin/Chart.jsx
@@ -1,58 +1,40 @@
 import React from "react";
 import Paper from '@material-ui/core/Paper';
-import DonutChart from "react-donut-chart";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 import Chart from 'react-google-charts';
 import { ThemeProvider } from "@material-ui/styles";
 
   
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['user', 'admin'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 2],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 export default function Chartx() {
- 
-    return (
-        <div style={{
-            backgroundColor:'#e12212',
-            borderBottom:12,
-            justifyContent:'center',
-            alignItems:'center',
-            paddingTop:25,
-            
-           
-            
-        }}>
-          <DonutChart 
-          
-        data={[
-          {
-            label: "Validasi Usulan",
-            value: 12
-          },
-          {
-            label: "Paraf Pertek",
-            value: 25
-          },
-          {
-            label: "Surat Keputusan",
-            value: 50
-          },
-          {
-            label: "",
-            value: 0,
-            isEmpty: true
-          }
-        ]}
-        options={{
-          responsive: true,
-          maintainAspectRatio: true,
-          
-          
-        }}
-        width={100}
-        backgroundColor={'F6F268'}
-        emptyColor={'3f10000'}
-        
-        
-      />
-        </div>
-      );
+  return <Doughnut data={data} />;
 }
-  
